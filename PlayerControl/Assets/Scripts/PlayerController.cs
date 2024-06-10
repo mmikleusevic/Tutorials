d486ce2a9f006101bab1eff53674dd2a9718c55a
mmikleusevic <mikleusevicmarin@gmail.com>
2024-06-10 15:32:40 +0200

    Added playerController script with moving forward

---
+++ PlayerControl/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        // Move the vehicle forward
+
+        transform.Translate(0, 0, 1);
+    }
+}
