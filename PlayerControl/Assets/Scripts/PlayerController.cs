80b2e1a74d1b6321496ce63a7a896015ce83d46f
mmikleusevic <mikleusevicmarin@gmail.com>
2024-06-11 14:57:53 +0200

    Added controls so that vehicle has turn speed

--- PlayerControl/Assets/Scripts/PlayerController.cs
+++ PlayerControl/Assets/Scripts/PlayerController.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 20f;
+    [SerializeField] private float turnSpeed = 2f;
 
     private void Update()
     {
         // Move the vehicle forward
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }
