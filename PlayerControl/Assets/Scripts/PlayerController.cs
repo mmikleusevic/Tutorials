2008c6ab6f3f975b7018d11a4acff99f45fb5d04
mmikleusevic <mikleusevicmarin@gmail.com>
2024-06-11 14:27:30 +0200

    Added speed variable to playerController

--- PlayerControl/Assets/Scripts/PlayerController.cs
+++ PlayerControl/Assets/Scripts/PlayerController.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float speed = 20f;
+
     void Update()
     {
         // Move the vehicle forward
 
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
