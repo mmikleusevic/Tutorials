585bd25dc78ae12b4ae491ec42b944c4492fa681
mmikleusevic <mikleusevicmarin@gmail.com>
2024-06-12 16:11:39 +0200

    Added spawnManager, spawning animals by pressing S key

--- BasicGameplay/Assets/Scripts/SpawnManager.cs
+++ BasicGameplay/Assets/Scripts/SpawnManager.cs
@@ -2,8 +2,16 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField] private GameObject[] animalPrefabs;
+    private int animalIndex = 0;
+
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            GameObject animalPrefab = animalPrefabs[animalIndex];
+
+            Instantiate(animalPrefab, new Vector3(0, 0, 20), animalPrefab.transform.rotation);
+        }
     }
 }
