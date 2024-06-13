6936573030b21ac8706dabe78d2ce1601dc3caf6
mmikleusevic <mikleusevicmarin@gmail.com>
2024-06-13 18:42:05 +0200

    Stopping spawning of obstacles on game over

--- Sound and Effects/Assets/Scripts/SpawnManager.cs
+++ Sound and Effects/Assets/Scripts/SpawnManager.cs
@@ -2,19 +2,26 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    private static string PLAYER = "Player";
+
     [SerializeField] private GameObject obstaclePrefab;
     [SerializeField] private float startDelay = 2f;
     [SerializeField] private float repeatRate = 2f;
 
+    private PlayerController playerController;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
 
     void Start()
     {
+        playerController = GameObject.FindWithTag(PLAYER).GetComponent<PlayerController>();
         InvokeRepeating(nameof(SpawnObstacle), startDelay, repeatRate);
     }
 
     private void SpawnObstacle() 
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if (playerController.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 }
