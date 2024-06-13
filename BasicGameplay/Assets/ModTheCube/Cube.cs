9264bf64afd6c36b75c91ddb0b9d04c5f0fc827c
mmikleusevic <mikleusevicmarin@gmail.com>
2024-06-13 14:41:56 +0200

    Added webGl build, added a new project

--- BasicGameplay/Assets/ModTheCube/Cube.cs
+++ BasicGameplay/Assets/ModTheCube/Cube.cs
@@ -1,23 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
+    [SerializeField] private float speed = 30f;
+
+    private float timer = 5f;
+    private float defaultTimer;
     
     void Start()
     {
-        transform.position = new Vector3(3, 4, 1);
-        transform.localScale = Vector3.one * 1.3f;
+        transform.position = new Vector3(Random.Range(0, 4), Random.Range(0, 4), Random.Range(0, 4));
+        transform.localScale = Vector3.one * Random.Range(1f, 3f);
         
         Material material = Renderer.material;
         
-        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+        material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 0.0f);
+
+        defaultTimer = timer;
     }
     
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        Material material = Renderer.material;
+
+        Color color = material.color;
+
+        timer -= Time.deltaTime;
+
+        if (timer > 0)
+        {
+            color.a += 0.0008f;
+        }
+        else
+        {
+            color.a -= 0.0008f;
+
+            if (timer < -defaultTimer)
+            {
+                timer = defaultTimer;
+            }
+        }
+
+        material.color = color;
+
+        transform.Rotate(10.0f * Time.deltaTime * speed, 10.0f * Time.deltaTime * speed, 10.0f  * Time.deltaTime * speed);
     }
 }
