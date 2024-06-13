4e54879ad63110c1469fa23a59f288f7f4ae95ea
mmikleusevic <mikleusevicmarin@gmail.com>
2024-06-13 18:48:49 +0200

    Destroying obstacles after it travels to more than -15

--- Sound and Effects/Assets/Scripts/MoveLeft.cs
+++ Sound and Effects/Assets/Scripts/MoveLeft.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private static string PLAYER = "Player";
+    private static string OBSTACLE = "Obstacle";
 
     [SerializeField] private float speed = 30f;
+    [SerializeField] private float leftBound = -15f;
 
     private PlayerController playerController;
 
@@ -18,6 +20,11 @@ public class MoveLeft : MonoBehaviour
         if (playerController.gameOver == false)
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (transform.position.x < leftBound && gameObject.CompareTag(OBSTACLE))
+        {
+            Destroy(gameObject);
         }
     }
 }
