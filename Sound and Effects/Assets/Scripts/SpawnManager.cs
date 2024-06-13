fd2b09f716a2d6fcbd5b75ce8cd44e7f861e2dd8
mmikleusevic <mikleusevicmarin@gmail.com>
2024-06-13 15:23:39 +0200

    Added instantiation of obstaclePrefab using spawnManager

--- Sound and Effects/Assets/Scripts/SpawnManager.cs
+++ Sound and Effects/Assets/Scripts/SpawnManager.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject obstaclePrefab;
+
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 }
