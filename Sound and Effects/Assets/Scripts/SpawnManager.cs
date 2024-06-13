909e87199422744c1e37e66adf4c08f0eb964244
mmikleusevic <mikleusevicmarin@gmail.com>
2024-06-13 15:30:36 +0200

    Changed start instantiation to invokeRepeating

--- Sound and Effects/Assets/Scripts/SpawnManager.cs
+++ Sound and Effects/Assets/Scripts/SpawnManager.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject obstaclePrefab;
+    [SerializeField] private float startDelay = 2f;
+    [SerializeField] private float repeatRate = 2f;
 
     private Vector3 spawnPos = new Vector3(25, 0, 0);
 
     void Start()
+    {
+        InvokeRepeating(nameof(SpawnObstacle), startDelay, repeatRate);
+    }
+
+    private void SpawnObstacle() 
     {
         Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
