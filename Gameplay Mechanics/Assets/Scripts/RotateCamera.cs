2dbdf62c8b8c13be5c2e0c3a220ddb3fdf977cd8
mmikleusevic <mikleusevicmarin@gmail.com>
2024-06-14 14:22:26 +0200

    Added code to rotateCamera to control focal point rotation

--- Gameplay Mechanics/Assets/Scripts/RotateCamera.cs
+++ Gameplay Mechanics/Assets/Scripts/RotateCamera.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
-    
+    [SerializeField] private float speed = 50f;
+
+    private float horizontalInput;
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, speed * Time.deltaTime * horizontalInput);
+    }
 }
