6060872d3f881e604399ecfd7fdfc4760485ebbd
mmikleusevic <mikleusevicmarin@gmail.com>
2024-06-14 14:49:50 +0200

    Added enemy and bouncy physics material

--- Gameplay Mechanics/Assets/Scripts/PlayerController.cs
+++ Gameplay Mechanics/Assets/Scripts/PlayerController.cs
@@ -5,15 +5,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed = 5f;
 
     private Rigidbody playerRb;
+    private GameObject focalPoint;
 
     private void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+        
     }
 
     private void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(Vector3.forward * speed * forwardInput * Time.deltaTime);
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput * Time.deltaTime);
     }
 }
