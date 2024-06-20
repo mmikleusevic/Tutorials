fd54ea97b992b8d0016a09e216e972e355e79330
mmikleusevic <mikleusevicmarin@gmail.com>
2024-06-20 13:27:13 +0200

    Added code to throw items up in the air and rotate them

--- UserInterface/Assets/Scripts/Target.cs
+++ UserInterface/Assets/Scripts/Target.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody targetRb;
+
+    private void Awake()
     {
-        
+        targetRb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
+        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
+
+        transform.position = new Vector3(Random.Range(-4, 4), -6);
     }
 }
