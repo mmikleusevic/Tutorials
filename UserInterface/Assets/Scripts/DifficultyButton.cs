720b6954db188694c094d017cf772aabdedb20f6
mmikleusevic <mikleusevicmarin@gmail.com>
2024-06-20 19:37:11 +0200

    Added scripts to difficulty buttons

---
+++ UserInterface/Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    private Button difficultyButton;
+
+    void Start()
+    {
+        difficultyButton = GetComponent<Button>();
+    }
+}
