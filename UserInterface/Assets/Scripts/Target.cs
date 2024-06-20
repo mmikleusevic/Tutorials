174a5938f7b85dbca6b717bea3ffc2b0455bdac2
mmikleusevic <mikleusevicmarin@gmail.com>
2024-06-20 13:34:10 +0200

    Cleaned up code

--- UserInterface/Assets/Scripts/Target.cs
+++ UserInterface/Assets/Scripts/Target.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private float minSpeed = 12f;
+    private float maxSpeed = 16f;
+    private float maxTorque = 10f;
+    private float xRange = 4;
+    private float yRange = -6;
 
     private void Awake()
     {
@@ -11,9 +16,24 @@ public class Target : MonoBehaviour
 
     private void Start()
     {
-        targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
-        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
 
-        transform.position = new Vector3(Random.Range(-4, 4), -6);
+        transform.position = RandomSpawnPosition();
+    }
+
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    private Vector3 RandomSpawnPosition()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), yRange);
     }
 }
