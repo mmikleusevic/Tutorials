6b3aca1d81411bf88cefea68b7b348cd49fab2d0
mmikleusevic <mikleusevicmarin@gmail.com>
2024-06-20 15:10:02 +0200

    Added scoreText to game manager and initialized it on start

--- UserInterface/Assets/Scripts/GameManager.cs
+++ UserInterface/Assets/Scripts/GameManager.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private List<GameObject> targets;
+    [SerializeField] private TextMeshProUGUI scoreText;
 
+    private int score;
     private float spawnRate = 1f;
 
     private void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        scoreText.text = "Score: " + score;
     }
 
     private IEnumerator SpawnTarget()
