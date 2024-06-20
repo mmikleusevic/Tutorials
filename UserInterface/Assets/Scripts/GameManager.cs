83480ded749f9606e41781cebc3e772b76d876f8
mmikleusevic <mikleusevicmarin@gmail.com>
2024-06-20 14:42:23 +0200

    Added courutine to spawn targets

--- UserInterface/Assets/Scripts/GameManager.cs
+++ UserInterface/Assets/Scripts/GameManager.cs
@@ -1,7 +1,26 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> targets;   
+    [SerializeField] private List<GameObject> targets;
+
+    private float spawnRate = 1f;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    private IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
 }
