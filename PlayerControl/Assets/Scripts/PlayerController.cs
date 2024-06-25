9de3f66a15e0af6d3ba5cf4adadbc83ad7328a37
mmikleusevic <mikleusevicmarin@gmail.com>
2024-06-26 01:51:27 +0200

    Added check for all wheels on ground so that we can properly update texts

--- PlayerControl/Assets/Scripts/PlayerController.cs
+++ PlayerControl/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,12 +11,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private GameObject centerOfMass;
     [SerializeField] private TextMeshProUGUI speedometerText;
     [SerializeField] private TextMeshProUGUI rpmText;
+    [SerializeField] private List<WheelCollider> wheelColliders;
 
     private Rigidbody rb;
     private float horizontalInput;
     private Vector2 moveDirection;
     private float speed;
     private float rpm;
+    private int wheelsOnGround;
 
     private void Awake()
     {
@@ -25,18 +28,37 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+ 
+
         moveDirection = playerMovement.ReadValue<Vector2>();
 
         // Moves the car forward based on vertical input
         rb.AddRelativeForce(Vector3.forward * Time.deltaTime * horsePower * moveDirection.y);
 
-        speed = Mathf.RoundToInt(rb.velocity.magnitude * 3.6f);
-        speedometerText.text = "Speed: " + speed + "km/h";
+        if (IsOnGround())
+        {
+            speed = Mathf.RoundToInt(rb.velocity.magnitude * 3.6f);
+            speedometerText.text = "Speed: " + speed + "km/h";
 
-        rpm = speed % 30 * 40;
-        rpmText.text = "PRM: " + rpm;
+            rpm = speed % 30 * 40;
+            rpmText.text = "PRM: " + rpm;
+        }
+
         // Rotates the car based on horizontal input
         transform.Rotate(GetTurningFactor());
+    }
+
+    private bool IsOnGround()
+    {
+        wheelsOnGround = 0;
+        foreach (WheelCollider collider in wheelColliders)
+        {
+            if (collider.isGrounded) wheelsOnGround++;
+        }
+
+        if (wheelsOnGround == 4) return true;
+
+        return false;
     }
 
     private void OnEnable()
