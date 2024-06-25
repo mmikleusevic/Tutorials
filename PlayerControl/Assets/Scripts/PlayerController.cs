e03b259c8bf8b6cf2ef0eee04b24925594ecab95
mmikleusevic <mikleusevicmarin@gmail.com>
2024-06-26 01:04:14 +0200

    Added using physics for player instead of transform

--- PlayerControl/Assets/Scripts/PlayerController.cs
+++ PlayerControl/Assets/Scripts/PlayerController.cs
@@ -3,19 +3,26 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private readonly float speed = 20f;
+    //[SerializeField] private readonly float speed = 20f;
+    [SerializeField] private float horsePower = 20f;
     [SerializeField] private float turnSpeed = 10f;
     [SerializeField] private InputAction playerMovement;
-    
+
+    private Rigidbody rb;
     private float horizontalInput;
     private Vector2 moveDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void FixedUpdate()
     {
         moveDirection = playerMovement.ReadValue<Vector2>();
 
         // Moves the car forward based on vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * moveDirection.y);
+        rb.AddRelativeForce(Vector3.forward * Time.deltaTime * horsePower * moveDirection.y);
         // Rotates the car based on horizontal input
         transform.Rotate(GetTurningFactor());
     }
