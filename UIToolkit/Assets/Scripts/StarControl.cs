9cb922976a4e4d87f4161fee555a10989fd0a2da
mmikleusevic <mikleusevicmarin@gmail.com>
2024-10-23 13:43:49 +0200

    Added custom control to custom project

---
+++ UIToolkit/Assets/Scripts/StarControl.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class StarControl : VisualElement
+{
+    public new class UxmlFactory : UxmlFactory<StarControl, UxmlTraits> { }
+
+    public new class UxmlTraits : VisualElement.UxmlTraits
+    {
+        UxmlIntAttributeDescription starAttribute = new UxmlIntAttributeDescription()
+        {
+            name = "stars"
+        };
+
+        public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
+        {
+            base.Init(ve, bag, cc);
+
+            (ve as StarControl).Stars = starAttribute.GetValueFromBag(bag, cc);
+        }
+    }
+
+    private const string styleResource = "StarControlStyle";
+    private const string ussContainer = "container";
+    private const string ussOuterStar = "outerStar";
+    private const string ussInnerStar = "innerStar";
+
+    List<VisualElement> outerStars = new List<VisualElement>();
+    List<VisualElement> innerStars = new List<VisualElement>();
+
+    private int stars;
+    public int Stars
+    {
+        get => stars;
+
+        set
+        {
+            stars = value;
+            SetStars();
+        }
+    }
+
+    public StarControl()
+    {
+        styleSheets.Add(Resources.Load<StyleSheet>(styleResource));
+
+        VisualElement container = new VisualElement();
+        hierarchy.Add(container);
+        container.AddToClassList(ussContainer);
+
+        for (int i = 0; i < 7; i++)
+        {
+            VisualElement outerStarTemp = new VisualElement();
+            outerStarTemp.name = "outerStar";
+            container.Add(outerStarTemp);
+            outerStarTemp.AddToClassList(ussOuterStar);
+
+            outerStars.Add(outerStarTemp);
+
+            VisualElement innerStarTemp = new VisualElement();
+            innerStarTemp.name = "innerStar";
+            outerStarTemp.Add(innerStarTemp);
+            innerStarTemp.AddToClassList(ussInnerStar);
+
+            innerStars.Add(innerStarTemp);
+        }
+
+        Stars = 0;
+    }
+
+    private void SetStars()
+    {
+        HideAllStars();
+
+        innerStars
+            .Take(stars)
+            .ToList()
+            .ForEach((elem) => elem.style.visibility = Visibility.Visible);
+    }
+
+    private void HideAllStars()
+    {
+        innerStars.ForEach((elem) => elem.style.visibility = Visibility.Hidden);
+    }
+}
